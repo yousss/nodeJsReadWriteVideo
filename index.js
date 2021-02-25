8f29fc408c32b01c3a615ea2f1a5be2ec2736c82
Yousss <yous.yoeunfrence@gmail.com>
2021-02-25 15:59:34 +0700

    readvideo

---
+++ index.js
@@ -0,0 +1,55 @@
+const { createServer } = require('http')
+const { stat, createReadStream, createWriteStream } = require('fs')
+const { promisify } = require('util')
+const fileName = './powder-day.mp4';
+const multiparty = require('multiparty')
+const fileNo = promisify(stat)
+
+const dealWithVideo = async (req, res) => {
+  const { size } = await fileNo(fileName)
+  const range = req.headers.range;
+  if (range) {
+    let [start, end] = range.replace(/bytes=/, '').split('-');
+    start = parseInt(start, 10)
+    end = end ? parseInt(end, 10) : size - 1;
+    res.writeHead(206, {
+      'Content-Range': `bytes ${start} - ${end}/${size}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': (end - start) + 1,
+      'Content-Type': 'video/mp4'
+    })
+    createReadStream(fileName, { start, end }).pipe(res)
+  } else {
+    res.writeHead(200,
+      { 'Content-Length': size },
+      { 'Content-Type': 'video/mp4' }
+    );
+    createReadStream(fileName).pipe(res)
+  }
+}
+
+createServer((req, res) => {
+  if (req.method === 'POST') {
+    let form = new multiparty.Form();
+    form.on('part', part => {
+      part.pipe(createWriteStream(`./${part.filename}`))
+        .on('close', () => {
+          res.writeHead(200, { 'Content-Type': 'text/html' })
+          res.end(`<h1>File uploaded: ${part.filename}`)
+        })
+    })
+    form.parse(req);
+  }
+  if (req.url === '/video') {
+    dealWithVideo(req, res);
+  }
+  else {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(`
+    <form enctype='multipart/form-data' method='POST' action='/'>
+      <input type='file' name='upload-file' />
+      <button>Upload file</button>
+    </form>
+    `)
+  }
+}).listen(4000, () => console.log('running...'))
